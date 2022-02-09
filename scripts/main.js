86423b6bc410911a7b707772ed48d5552d6dd23c
JustinLung <justinlung77@gmail.com>
2022-02-09 11:59:14 +0100

    WIP

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
+// VARIABLES
 const API_URL = 'https://tribe.api.fdnd.nl/v1';
 const nameEl = document.querySelector(".name");
 const backName = document.querySelector(".name-surname");
 const image = document.getElementById("profileImage");
 const bioEl = document.getElementById("bio");
 const githubHandleEL = document.getElementById("githubHandle");
+const urlEL = document.getElementById("url");
 const card = document.querySelector("#card");
 
 getMember();
@@ -11,18 +13,14 @@ card.addEventListener("click", function () {
     this.classList.toggle("card-rotate")
 })
 
+// FUNCTIONS
 async function getMember() {
-    const req = await fetch(`${API_URL}/member`)
-    console.log(req)
-    const member = await req.json()
+    const req = await fetch(`${API_URL}/member`);
+    const member = await req.json();
     nameEl.innerText = member.data[0].name;
-    backName.innerText = member.data[0].name + " " + member.data[0].surname
-    console.log(member.data)
-}
+    backName.innerText = `${member.data[0].name} ${member.data[0].surname}`
+    githubHandleEL.href = member.data[0].githubHandle;
+    urlEL.href = member.data[0].url;
+    image.src = member.data[0].avatar;
 
-async function fillMember() {
-    fetch(`${API_URL}/member`), {
-        method: "PATCH",
-
-    }
 }
